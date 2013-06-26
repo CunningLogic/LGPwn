e010f519a5a0592ba44a2cf340459eb48a8b53be
Justin Case <jcase@cunninglogic.com>
2013-06-25 17:10:31 -0700

    Greater device compatibility

--- src/com/cunninglogic/lgpwn/MainActivity.java
+++ src/com/cunninglogic/lgpwn/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cunninglogic.lgpwn;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -11,19 +13,27 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		File spritebud = new File("/system/xbin/spritebud");
+		
+		
 		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-		dialog.setTitle("Guide");
-		dialog.setMessage("Press RootMe to run the backup app, then select restore, then internal storage," +
-				" then restore the Lgpwn backup. Shortly after the restore is done, the phone will reboot." +
-				" once the phone has rebooted, please install Supersu from the market.");
-	
+		dialog.setTitle("CVE-2013-3685 POC by jcase"); 
 		dialog.setCancelable(false);
-		dialog.setPositiveButton("RootMe",new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog,int id) {
-
-				Worker.setup(MainActivity.this);
-			}
-		  });
+		
+		if (spritebud.exists()) {
+			dialog.setMessage("Press RootMe to run the backup app, then select restore, then internal storage," +
+					" then restore the Lgpwn backup. Shortly after the restore is done, the phone will reboot." +
+					" once the phone has rebooted, please install Supersu from the market.");
+			dialog.setPositiveButton("RootMe",new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,int id) {
+	
+					Worker.setup(MainActivity.this);
+				}
+			  });
+		} else {
+			dialog.setMessage("Can not find /system/xbin/spritebud, device may not be vulnerable.");
+		}
 		dialog.setNegativeButton("Exit",new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog,int id) {
 				MainActivity.this.finish();
